98e94e3bd60e5bd3c5b6e8573b51dde6cbb51375
Aryan Ratra <95769010+aryanratra@users.noreply.github.com>
2022-07-16 01:11:37 +0530

    Added Image switching

--- 360-VR-Viewer/scripts/script.js
+++ 360-VR-Viewer/scripts/script.js
@@ -13,7 +13,9 @@ let galleryImages = [
     './images/image5.jpg',
 ];
 
-let viewerContainer = document.getElementById('webglviewer');
+let viewerContainer = document.getElementById('viewer');
+
+let gallery2D = document.getElementById('thumbnail-gallery');
 
 let clock = new THREE.Clock();
 
@@ -24,22 +26,37 @@ let textureLoader;
 let viewer;
 let controls;
 
+function addToGallery2D(texture) {
+    let thumbnail = document.createElement('div');
+    thumbnail.classList.add('thumbnail');
+    //thumbnail.appendChild(texture.image);
+    thumbnail.addEventListener('click', function() {
+      viewer.material.map = texture;
+    });
+    gallery2D.appendChild(thumbnail);
+  }
+
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100000);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     textureLoader = new THREE.TextureLoader();
 
     //Image Viewer
     let viewerGeometry = new THREE.SphereGeometry(500, 60, 40);
     viewerGeometry.scale(-1, 1, 1);
-
-    let viewerMaterial = new THREE.MeshBasicMaterial({
-        map: textureLoader.load(galleryImages[0])
-    });
-
+    
+    
+    let viewerMaterial = new THREE.MeshBasicMaterial({map: textureLoader.load(galleryImages[0])});
+    
     viewer = new THREE.Mesh(viewerGeometry, viewerMaterial);
     viewer.rotation.y = -Math.PI / 2;
     scene.add(viewer);
+    
+    //Thumbnail Gallery
+    for (let i = 0; i < galleryImages.length; ++i) {
+        let texture = textureLoader.load(galleryImages[i]);
+        addToGallery2D(texture);
+      }
 
     //Initialize the Renderer
     renderer = new THREE.WebGLRenderer({ antialias: true });
